78116c4eda1e43f33c092ec054808988f5529cf8
Mx-Crvr <mxcrvr31@gmail.com>
2023-03-06 16:49:59 +0200

    added active states to buttons

--- app.js
+++ app.js
@@ -1,34 +1,52 @@
 const buttons = document.querySelectorAll('button');
 const author = document.getElementById('author');
 const quote = document.getElementById('quote');
-
-
-
-
-async function updateQuote(url) {
-    // Fetch a random quote from the Quotable API
-    const response = await fetch(`https://api.quotable.io/random?=tags${url}`);
-    const data = await response.json();
-    if (response.ok) {
-      // Update DOM elements
-      quote.textContent = data.content;
-      author.textContent = data.author;
-    } else {
-      quote.textContent = "An error occured";
-      console.log(data);
-    }
-  }
-
-
-  function callFunction() {
-    console.log(this.id)
-    updateQuote(this)
-    
-  }
+const nextBtn = document.getElementById('next-btn');
+const quoteWrapper = document.getElementById('quote-wrapper')
 
   for (const button of buttons) {
-    button.classList.add = 'active';
-    button.addEventListener('click', (callFunction))
-    };
+  
+    button.addEventListener('click', function () {
+      buttons.forEach(button => {
+        button.classList.remove('active');
+        button.disabled = false;
+        quoteWrapper.style.display = 'flex';
+      });
+      button.classList.add('active');
+      button.setAttribute('disabled', '')
+  
+      async function updateQuote() {
+        let tag = `?tags=${button.id}`;
+        const response = await fetch(`https://api.quotable.io/random${tag}`);
+        console.log(response)
+        const data = await response.json();
+        if (response.ok) {
+          quote.textContent = data.content;
+          author.textContent = data.author;
+        } else {
+          quote.textContent = "An error occured";
+          console.log(data);
+        }
+  
+      }
+      updateQuote()
+    }) 
+  
+  };
+
+
+// function resetBtn() {
+//   for (const button of buttons) {
+//     button.style.backgroundColor = '#90C26E'
+//   }
+// }
+
+
+
+
+
+
+
+ 
 
    
